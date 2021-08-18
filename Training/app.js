f978594f3354c202cbe559f5f1ba8ff7b7205bf7
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-18 18:55:14 +0200

    EX 4 done

--- Training/app.js
+++ Training/app.js
@@ -1,15 +1,38 @@
-function greaterThan(n) {
-  return (m) => m > n;
+function filter(array, test) {
+  let passed = [];
+  for (let element of array) {
+    if (test(element)) {
+      passed.push(element);
+    }
+  }
+  return passed;
 }
-let greaterThan10 = greaterThan(10);
-console.log(greaterThan10(11));
 
-function noisy(f) {
-  return (...args) => {
-    console.log(`calling with ${args}`);
-    let result = f(...args);
-    console.log(`called with ${args} returned ${result}`);
-    return result;
-  };
+function map(array, transform) {
+  let mapped = [];
+  for (let element of array) {
+    mapped.push(transform(element));
+  }
+  return mapped;
 }
-noisy(Math.min)(3, 2, 1);
+
+function reduce(array, combine, start) {
+  let current = start;
+  for (let element of array) {
+    current = combine(current, element);
+  }
+  return current;
+}
+
+function characterCount(script) {
+  return script.range.reduce((count, [from, to]) => {
+    return count + (to - from);
+  }, 0);
+}
+
+function average(array) {
+  return array.reduce((a, b) => a + b) / array.length;
+}
+
+// Math.round(average(SCRIPTS.filter(s => s.living).map(s => s.year)))
+// Math.round(average(SCRIPTS.filter(s => !s.living).map(s => s.year)))
