07cf67c4cb22308daeebff9ed700b6e2f107d4e3
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-18 18:12:06 +0200

    pratice exeamples

--- Training/app.js
+++ Training/app.js
@@ -1,44 +1,15 @@
-function arrayToList(arr) {
-  let list = null;
-  for (let i = arr.length; i >= 0; i--) {
-    list = { value: arr[i], rest: list };
-  }
-  return list;
+function greaterThan(n) {
+  return (m) => m > n;
 }
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
 
-console.log(arrayToList([10, 20]));
-
-function listToArray(list) {
-  let arr = [];
-  for (let node = list; node; node = node.rest) {
-    arr.push(node.value);
-  }
-  return arr;
+function noisy(f) {
+  return (...args) => {
+    console.log(`calling with ${args}`);
+    let result = f(...args);
+    console.log(`called with ${args} returned ${result}`);
+    return result;
+  };
 }
-
-console.log(
-  listToArray({
-    value: 10,
-    rest: {
-      value: 20,
-      rest: {
-        value: 30,
-        rest: null,
-      },
-    },
-  })
-);
-
-function prepend(elem, list) {
-  return (list = { value: elem, rest: list });
-}
-console.log(prepend(10, prepend(20, null)));
-
-function nth(list, n) {
-  if (!list) return undefined;
-  else if (n == 0) return list.value;
-  else {
-    return nth(list.rest, n - 1);
-  }
-}
-console.log(nth(arrayToList([10, 20, 30]), 2));
+noisy(Math.min)(3, 2, 1);
