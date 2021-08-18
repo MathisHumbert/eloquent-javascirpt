cd486fed4548b75b50cb25b6b831c10eceefdbf1
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-18 17:26:11 +0200

    redoing ex3

--- Training/app.js
+++ Training/app.js
@@ -1,20 +1,44 @@
-const reverseArray = (arr) => {
-  let newArr = [];
-  for (let i of arr) {
-    newArr.unshift(i);
+function arrayToList(arr) {
+  let list = null;
+  for (let i = arr.length; i >= 0; i--) {
+    list = { value: arr[i], rest: list };
   }
-  return newArr;
-};
-console.log(reverseArray(['A', 'B', 'C']));
+  return list;
+}
 
-const reverseArrayInPlace = (arr) => {
-  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-    let old = arr[i];
-    arr[i] = arr[arr.length - 1 - i];
-    arr[arr.length - 1 - i] = old;
+console.log(arrayToList([10, 20]));
+
+function listToArray(list) {
+  let arr = [];
+  for (let node = list; node; node = node.rest) {
+    arr.push(node.value);
   }
   return arr;
-};
-let arrayValue = [1, 2, 3, 4, 5];
-reverseArrayInPlace(arrayValue);
-console.log(arrayValue);
+}
+
+console.log(
+  listToArray({
+    value: 10,
+    rest: {
+      value: 20,
+      rest: {
+        value: 30,
+        rest: null,
+      },
+    },
+  })
+);
+
+function prepend(elem, list) {
+  return (list = { value: elem, rest: list });
+}
+console.log(prepend(10, prepend(20, null)));
+
+function nth(list, n) {
+  if (!list) return undefined;
+  else if (n == 0) return list.value;
+  else {
+    return nth(list.rest, n - 1);
+  }
+}
+console.log(nth(arrayToList([10, 20, 30]), 2));
