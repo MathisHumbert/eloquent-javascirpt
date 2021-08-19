e7a9ebf4d3a9af5526758fae885222f42c955e20
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-19 17:57:30 +0200

    EX 2 done

--- Chapter-5/EX-2/app.js
+++ Chapter-5/EX-2/app.js
@@ -1,1 +1,15 @@
 // My way
+function loop(value, func, iter, body) {
+  if (func(value)) {
+    body(value);
+    value = iter(value);
+    loop(value, func, iter, body);
+  } else return;
+}
+
+loop(
+  3,
+  (n) => n > 0,
+  (n) => n - 1,
+  console.log
+);
