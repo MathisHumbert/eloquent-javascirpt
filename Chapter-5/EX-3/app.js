2eb55ebba81d77e54a04c85e31691d98c3830d2f
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-19 18:07:23 +0200

    EX 3 done

--- Chapter-5/EX-3/app.js
+++ Chapter-5/EX-3/app.js
@@ -1,1 +1,15 @@
 // My way
+function every(array, test) {
+  for (let i of array) {
+    if (!test(i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function every(array, test) {
+  return !array.some((arr) => !test(arr));
+}
+console.log(every([1, 3, 5], (n) => n < 10));
+console.log(every([2, 4, 16], (n) => n < 10));
