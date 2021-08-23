b60721cc7c6c80ecc180f25ff86cf3743942b439
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-23 18:39:11 +0200

    examples from the book

--- Training/app.js
+++ Training/app.js
@@ -31,8 +31,14 @@ function Rabbit(type) {
 Rabbit.prototype.speak = function (line) {
   console.log(`The ${this.type}rabbit says '${line}'`);
 };
+Rabbit.prototype.teeth = 'small';
 let weirdRabbit = new Rabbit('weird');
-console.log(weirdRabbit);
+let blackRabbit = new Rabbit('black');
+console.log(weirdRabbit.teeth);
+killerRabbit.teeth = 'long';
+console.log(killerRabbit.teeth);
+console.log(blackRabbit.teeth);
+console.log(Rabbit.prototype.teeth);
 
 class RabbitC {
   constructor(type) {
@@ -44,3 +50,22 @@ class RabbitC {
 }
 let weirdCRabbit = new RabbitC('weird');
 console.log(weirdCRabbit);
+
+let ages = {
+  Boris: 39,
+  Liang: 22,
+  Julia: 62,
+};
+console.log(`Julia is ${ages['Julia']}`);
+console.log(`Is Jack's age known ?`, 'Jack' in ages);
+console.log(`Is toString's age known ?`, 'toString' in ages);
+console.log('toString' in Object.create(null));
+
+let ages2 = new Map();
+ages2.set('Boris', 39);
+ages2.set('Liang', 22);
+ages2.set('Julia', 62);
+console.log(ages2);
+console.log(`Julia is ${ages2.get('Julia')}`);
+console.log(`Is Jack's age known ?`, ages2.has('Jack'));
+console.log(`Is toString's age known ?`, ages2.has('toString'));
