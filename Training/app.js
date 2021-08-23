c1419c77338980f94ee03f8d610e52dcffa0412c
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-23 18:23:11 +0200

    examples from the book

--- Training/app.js
+++ Training/app.js
@@ -11,3 +11,36 @@ function normalize() {
   console.log(this.coords.map((n) => n / this.length));
 }
 normalize.call({ coords: [0, 2, 3], length: 5 });
+
+console.log(Object.prototype);
+console.log(Object.getPrototypeOf(Math.max));
+console.log(Object.getPrototypeOf([]));
+
+let protoRabbit = {
+  speak(line) {
+    console.log(`The ${this.type}rabbit says '${line}'`);
+  },
+};
+let killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = 'killer';
+killerRabbit.speak('SKEEEE');
+
+function Rabbit(type) {
+  this.type = type;
+}
+Rabbit.prototype.speak = function (line) {
+  console.log(`The ${this.type}rabbit says '${line}'`);
+};
+let weirdRabbit = new Rabbit('weird');
+console.log(weirdRabbit);
+
+class RabbitC {
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`The ${this.type}rabbit says '${line}'`);
+  }
+}
+let weirdCRabbit = new RabbitC('weird');
+console.log(weirdCRabbit);
