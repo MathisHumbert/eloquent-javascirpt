1d01cc5596bb481ea75817c03218ca97e2ec4861
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-25 17:33:21 +0200

    ex 1 done

--- Chapter-6/EX-1/app.js
+++ Chapter-6/EX-1/app.js
@@ -1,7 +1,20 @@
 // My way
-function Vec(x, y) {
-  this.x = x;
-  this.y = y;
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  plus(a, b) {
+    return new Vec(this.x + a, this.y + b);
+  }
+  minus(a, b) {
+    return new Vec(this.x - a, this.y - b);
+  }
+  get length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
 }
 
-console.log(new Vec(1, 2));
+console.log(new Vec(1, 2).plus(2, 3));
+console.log(new Vec(1, 2).minus(2, 3));
+console.log(new Vec(3, 4).length);
