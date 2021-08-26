061686ccb960f8bfad83e0aeae7dc85652cb22f2
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-26 17:37:10 +0200

    practicing examples

--- Training/app.js
+++ Training/app.js
@@ -1,36 +1,62 @@
-let rabbit = {
-  speak: function (line) {
-    console.log(`The rabbit says ${line}`);
-  },
+const toStringSymbol = Symbol('toString');
+Array.prototype[toStringSymbol] = function () {
+  return `${this.length} cm of clue yarn`;
 };
-rabbit.speak('World');
+console.log([1, 2].toString());
+console.log([1, 2][toStringSymbol]());
 
-function speak(line) {
-  console.log(`The ${this.type} rabbit says ${line}`);
-}
+let okIterator = 'OK'[Symbol.iterator]();
+console.log(okIterator.next());
+console.log(okIterator.next());
+console.log(okIterator.next());
 
-let whiteRabbit = { type: 'white', speak };
-let blackRabbit = { type: 'black', speak };
-whiteRabbit.speak('World');
-whiteRabbit.speak('World');
+class Matrix {
+  constructor(width, height, element = (x, y) => undefined) {
+    this.width = width;
+    this.height = height;
+    this.content = [];
 
-function Rabbit(type) {
-  this.type = type;
-}
-Rabbit.prototype.speak = function (line) {
-  console.log(`The ${this.type} rabbit says ${line}`);
-};
-
-let weirdRabbit = new Rabbit('weird');
-weirdRabbit.speak('lleamnzv');
-
-class RabbitBis {
-  constructor(type) {
-    this.type = type;
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        return this.content[y * this.width + x];
+      }
+    }
   }
-  speak(line) {
-    console.log(`The ${this.type} rabbit says ${line}`);
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
   }
 }
-let killerRabbit = new RabbitBis('killer');
-killerRabbit.speak('crazy');
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+  next() {
+    if (this.y == this.matrix.height) return { done: true };
+
+    let value = {
+      x: this.x,
+      y: this.y,
+      value: this.matrix.get(this.x, this.y),
+    };
+    this.x++;
+    if (this.x == this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+    return { value, done: false };
+  }
+}
+Matrix.prototype[Symbol.iterator] = function () {
+  return new MatrixIterator(this);
+};
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+for (let { x, y, value } of matrix) {
+  console.log(x, y, value);
+}
