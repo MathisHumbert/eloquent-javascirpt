e06676c750da9381825538c43966efa1b9c909e3
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-08-30 18:22:41 +0200

    project robot examples

--- Training/app.js
+++ Training/app.js
@@ -1,109 +1,35 @@
-const toStringSymbol = Symbol('toString');
-Array.prototype[toStringSymbol] = function () {
-  return `${this.length} cm of clue yarn`;
-};
-console.log([1, 2].toString());
-console.log([1, 2][toStringSymbol]());
+var roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  'Marketplace-Farm',
+  'Marketplace-Post Office',
+  'Marketplace-Shop',
+  'Marketplace-Town Hall',
+  'Shop-Town Hall',
+];
 
-let okIterator = 'OK'[Symbol.iterator]();
-console.log(okIterator.next());
-console.log(okIterator.next());
-console.log(okIterator.next());
-
-class Matrix {
-  constructor(width, height, element = (x, y) => undefined) {
-    this.width = width;
-    this.height = height;
-    this.content = [];
-
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        return this.content[y * this.width + x];
-      }
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
     }
   }
-  get(x, y) {
-    return this.content[y * this.width + x];
+  for (let [from, to] of edges.map((r) => r.split('-'))) {
+    addEdge(from, to);
+    addEdge(to, from);
   }
-  set(x, y, value) {
-    this.content[y * this.width + x] = value;
-  }
+  return graph;
 }
 
-class MatrixIterator {
-  constructor(matrix) {
-    this.x = 0;
-    this.y = 0;
-    this.matrix = matrix;
-  }
-  next() {
-    if (this.y == this.matrix.height) return { done: true };
-
-    let value = {
-      x: this.x,
-      y: this.y,
-      value: this.matrix.get(this.x, this.y),
-    };
-    this.x++;
-    if (this.x == this.matrix.width) {
-      this.x = 0;
-      this.y++;
-    }
-    return { value, done: false };
-  }
-}
-Matrix.prototype[Symbol.iterator] = function () {
-  return new MatrixIterator(this);
-};
-
-let matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
-for (let { x, y, value } of matrix) {
-  console.log(x, y, value);
-}
-
-console.clear();
-
-let varyingSize = {
-  get size() {
-    return Math.floor(Math.random() * 100);
-  },
-};
-console.log(varyingSize.size);
-console.log(varyingSize.size);
-
-class Temperature {
-  constructor(celsius) {
-    this.celsius = celsius;
-  }
-  get fahrenheit() {
-    return this.celsius * 1.8 + 32;
-  }
-  set fahrenheit(value) {
-    this.celsius = (value - 32) / 1.8;
-  }
-  static fromFahrenheit(value) {
-    return new Temperature((value - 32) / 1.8);
-  }
-}
-
-let temp = new Temperature(22);
-console.log(temp.fahrenheit);
-console.log(temp.celsius);
-
-class SymmetricMatrix extends Matrix {
-  constructor(size, element = (x, y) => undefined) {
-    super(size, size, (x, y) => {
-      if (x < y) return element(y, x);
-      else return element(x, y);
-    });
-  }
-  set(x, y, value) {
-    super.set(x, y, value);
-    if (x != y) {
-      super.set(y, x, value);
-    }
-  }
-}
-
-let matrixBis = new SymmetricMatrix(5, (x, (y) => `${x}, ${y}`));
-console.log(matrixBis.get(2, 4));
+const roadGraph = buildGraph(roads);
+console.log(roadGraph);
